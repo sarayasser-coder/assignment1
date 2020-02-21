4bca11f697bbb53dfd9d1a8509ed11f46c7f86bf
sarayasser-coder <58732317+sarayasser-coder@users.noreply.github.com>
2020-02-21 23:33:47 +0200

    Add files via upload

--- functions.java
+++ functions.java
@@ -3,12 +3,18 @@ package calculator;
 public class functions {
 
 	int add(int x, int y) {
-		return x+y;
+		return x + y;
 		
 	}
 	float divide(int x, int y) throws RuntimeException {
 		
-		return x/y;
+		if (y == 0)
+		{
+			throw new RuntimeException("can't divide by zero!");
+		}
+		else
+		{
+		return (float) x/y;
+		}
 	}
-
 }
